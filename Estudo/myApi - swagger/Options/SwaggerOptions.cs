b592d567eef700f84edb9626b040cdf9a6e32df4
Anderson Conceição <andersonconceiicao@gmail.com>
2020-05-08 18:04:17 -0300

    Subindo proj

---
+++ Estudo/myApi - swagger/Options/SwaggerOptions.cs
@@ -0,0 +1,9 @@
+namespace myapi.Options
+{
+    public class SwaggerOptions
+    {
+        public string JsonRouter { get; set; }
+        public string Description { get; set; }
+        public string UIEndPoint { get; set; }
+    }
+}
