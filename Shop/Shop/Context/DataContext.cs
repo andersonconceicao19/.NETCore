525a39aff9cbdcc8d6a9ec641f7f68589bfbdc28
Anderson Conceição <andersonconceiicao@gmail.com>
2020-08-15 14:36:18 -0300

    crud das categorias feito

---
+++ Shop/Shop/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Shop/Shop/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Context;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("")]
+        public async Task<ActionResult<List<Category>>> Get()
+        {
+            return Ok(await _context.Categories.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Category>> GetbyId(int id)
+        {
+            var result = await _context.Categories.FindAsync(id);
+            if (result == null) return NotFound();            
+
+            return Ok(result);
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult> Post([FromBody]Category model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            _context.Categories.Add(model);
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody]Category model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            if (id != model.Id) return NotFound();
+
+            _context.Entry<Category>(model).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var result = await _context.Categories.FindAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(result);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
