2f43ae41a7bdad6e53af1679975f052e0830b955
Anderson Conceição <andersonconceiicao@gmail.com>
2020-08-17 22:21:44 -0300

    configurando autenticação

---
+++ Shop/Shop/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "daafd680-4e2b-47d2-bf28-87040fa20e14";
+    }
+}
